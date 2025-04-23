8b3ff18711ec78afece3bd7d77597572fd23b18a
harcop <harcopdrew@gmail.com>
2025-04-24 00:40:10 +0100

    add intro

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup></script>
+<style>
+* {
+  font-family: 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+  margin: auto;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h1>About Toluwap</h1>
+    <p>
+      <img src="~/assets/images/toluwap.png" alt="Toluwap" class="rounded-full" width="200" height="200">
+    </p>
+    <p>Toluwap is an experienced software engineer and a mentor. He is the developer of the famous Kokonut and Kuest.io. He has developed, touched, deployed, maintained some of the great projects that have shaped the face of the digital space. He's currently a senior software engineer at BBC and a proud family man.</p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 300;
+  font-family: Lora;
+}
+.rounded-full {
+  border-radius: 5%;
+}
+</style>
