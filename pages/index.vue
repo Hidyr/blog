f5ecb97b6e20bef291d7e0bc2e705dea69aecb2f
harcop <harcopdrew@gmail.com>
2025-04-24 13:38:59 +0100

    add intro page

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="container">
-    <h1>About Toluwap</h1>
-    <p>
-      <img src="~/assets/images/toluwap.png" alt="Toluwap" class="rounded-full" width="200" height="200">
-    </p>
-    <p>Toluwap is an experienced software engineer and a mentor. He is the developer of the famous Kokonut and Kuest.io. He has developed, touched, deployed, maintained some of the great projects that have shaped the face of the digital space. He's currently a senior software engineer at BBC and a proud family man.</p>
+    <div>
+      <NuxtLink to="/cnsd">
+        <h2>Deploy Nuxt Static site to Cloudflare</h2>
+        <p>You want to quickly show case your application to a friend, potential users or to launch a simple MVP out there but stuck up with multiple options of hosting platforms, pricing, domain name and endless of other relating components like SSL...</p>
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
+<script setup></script>
 <style scoped>
-h1 {
-  font-weight: 300;
-  font-family: Lora;
-}
-.rounded-full {
-  border-radius: 5%;
+a {
+  text-decoration: none;
+  color: #292929;
 }
 </style>
